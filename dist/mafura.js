8badd6c51690de62ab4fbe1e514cabb4ffcc7930
Julianto Chai <saijnawazaki@gmail.com>
2020-02-22 02:22:57 +0700

    Define CSS Variables

--- dist/mafura.js
+++ dist/mafura.js
@@ -3,6 +3,12 @@
 */
 
 window.onload = function(){
+
+  if(window.pageYOffset == 0)
+  {
+      document.getElementById('navbar').className = document.getElementById('navbar').className.replace(/\bnavbar-dark\b/g, "navbar-transparent");
+  }
+
   var navbarMenuButton = document.getElementsByClassName('navbar-menu-button');
 
   function toggleNavbarMenuButton(there)
@@ -24,4 +30,47 @@ window.onload = function(){
   for (var i = navbarMenuButton.length; i--;) {
     navbarMenuButton[i].addEventListener('click', function(){toggleNavbarMenuButton(this)}, false);
   }
+
+  let intervalId = 0;
+  let scrollButton = document.getElementById('button_back_to_top');
+
+  function scrollStep() {
+      // Check if we're at the top already. If so, stop scrolling by clearing the interval
+      if (window.pageYOffset === 0) {
+          clearInterval(intervalId);
+      }
+      window.scroll(0, window.pageYOffset - 50);
+  }
+
+  function scrollToTop() {
+      // Call the function scrollStep() every 16.66 millisecons
+      intervalId = setInterval(scrollStep, 16.66);
+  }
+
+  // When the DOM is loaded, this click handler is added to our scroll button
+  scrollButton.addEventListener('click', scrollToTop);
+
+  var prevScrollpos = window.pageYOffset;
+
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if(currentScrollPos == 0)
+    {
+        document.getElementById('navbar').className = document.getElementById('navbar').className.replace(/\bnavbar-dark\b/g, "navbar-transparent");
+    }
+    else
+    {
+        document.getElementById('navbar').className = document.getElementById('navbar').className.replace(/\bnavbar-transparent\b/g, "navbar-dark");
+    }
+
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("button_back_to_top").style.bottom = "-100px";
+
+    } else {
+      document.getElementById("button_back_to_top").style.bottom = "20px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
 }
