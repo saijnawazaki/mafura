6cd43fb013e768a68d99e15b25f5aa85e1a3a4ef
Julianto Chai <saijnawazaki@gmail.com>
2022-12-09 16:54:53 +0700

    init

---
+++ factory/config.php
@@ -0,0 +1,13 @@
+<?php
+define('APP_CSS_OUTPUT','../dist/mafura.css');
+
+$app_theme_color = array(
+    'primary' => 'hsl(216, 98%, 52%)',
+    'secondary' => 'hsl(208, 7%, 46%)',
+    'success',
+    'warning',
+    'danger',
+    'info',
+    'white',
+    'black',
+);
